d08923352363b116f824a05e68b32e7f62d56370
GIOVANNAZ\gzccl <giovanna.zaccolon@littlemaker.com.br>
2023-06-25 19:07:32 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+  isHappy: boolean = true;
+  isSad: boolean = false;
+  foodScore: number = 0;
+  happinessScore: number = 100;
+  feedingInterval: any;
+  clothes: string = 'default';
 
-  constructor() {}
+  constructor() {
+    this.feedingInterval = setInterval(() => {
+      this.happinessScore -= 10;
+      this.foodScore += 10;
 
+      if (this.happinessScore <= 0 || this.foodScore >= 100) {
+        this.isHappy = false;
+        this.isSad = true;
+        clearInterval(this.feedingInterval);
+      }
+    }, 10000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.feedingInterval);
+  }
+
+  feedBichinho() {
+    this.foodScore -= 20;
+    this.happinessScore += 10;
+    this.checkBichinhoStatus();
+  }
+
+  petBichinho() {
+    this.happinessScore += 20;
+    this.checkBichinhoStatus();
+  }
+
+  takeBichinhoForAWalk() {
+    this.happinessScore += 30;
+    this.foodScore += 10;
+    this.checkBichinhoStatus();
+  }
+
+  changeBichinhoClothes() {
+    if (this.clothes === 'default') {
+      this.clothes = 'shirt';
+    } else if (this.clothes === 'shirt') {
+      this.clothes = 'hat';
+    } else if (this.clothes === 'hat') {
+      this.clothes = 'default';
+    }
+  }
+
+  checkBichinhoStatus() {
+    if (this.happinessScore > 0 && this.foodScore < 100) {
+      this.isHappy = true;
+      this.isSad = false;
+    } else {
+      this.isHappy = false;
+      this.isSad = true;
+    }
+  }
 }
